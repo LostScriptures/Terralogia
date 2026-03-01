39f2ebefbc4620127e20652644f048c0a9877381
LostScriptures <lost.player.game@gmail.com>
2026-03-01 14:22:08 +0100

    some more

--- SW-Architectur/DeviceTest.cpp
+++ SW-Architectur/DeviceTest.cpp
@@ -2,6 +2,8 @@
 #include <thread>
 #include <queue>
 #include <vector>
+#include <chrono>
+#include <format>
 
 #include "Devices.h"
 #include "DeviceManager.h"  
@@ -39,9 +41,12 @@ int main() {
     cout << "Event pushed to queue\n";
     
     Event result = in_queue.wait_and_pop().unwrap();
-    cout << "Event processed with state: " << result.state << "\n";
-    cout << "Event start time: " << result.start_time.count() << " ms\n";
-    cout << "Event end time: " << result.end_time.count() << " ms\n";
+    chrono::system_clock::time_point start_tp = chrono::system_clock::time_point(chrono::milliseconds(result.start_time));
+    chrono::system_clock::time_point end_tp = chrono::system_clock::time_point(chrono::milliseconds(result.end_time));
+
+    cout << "Event processed with state: " << result.state << endl;
+    cout << "Event start time: " << format("{:.22s}", format("{:%F %T}", start_tp)) << endl;
+    cout << "Event end time: " << format("{:.22s}", format("{:%F %T}", end_tp)) << endl;
     
     return 0;
 }
--- SW-Architectur/Devices.cpp
+++ SW-Architectur/Devices.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <format>
 
 #include "Devices.h"
 #include "DataClasses.h"
@@ -13,7 +14,13 @@ State Lights::update(const Event& task) {
 }
 
 State Dummy::update(const Event& task)  {
-    cout << "ID: " << task.targetId << "\nSTAT: " << task.state << "\nCOM: " << task.command << "\nVAL: " << task.value << endl;
+    chrono::system_clock::time_point tp = chrono::system_clock::time_point(chrono::milliseconds(task.start_time));
+    cout << "-- Dummy Event (" << format("{:.22s}", format("{:%F %T}", tp)) << ") --\n";
+    cout << "ID: " << task.targetId << endl;
+    cout << "STAT: " << task.state << endl;
+    cout << "COM: " << task.command << endl;
+    cout << "VAL: " << task.value << endl;
+    
     this_thread::sleep_for(chrono::seconds(5));
     return State::DONE;
 };
